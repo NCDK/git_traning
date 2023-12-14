a8abf7826d0cdc3d9ffe81ade1e77c033af5c159
NCDK <145363398+NCDK@users.noreply.github.com>
2023-12-14 13:26:44 +0100

    Update Githubtest.R

--- Githubtest.R
+++ Githubtest.R
@@ -3,7 +3,3 @@ library(jsonlite)
 # Just a quick push to see if we can merge it like a G
 ## huhuhuhuu
 # f
-=======
-# Just a quick push 4
-# ugiorguero
->>>>>>> adb66594e0cab24331ad125a4d0538ee2db8b255
