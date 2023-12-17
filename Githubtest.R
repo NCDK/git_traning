68d8871b0a8e0255bddb5b7271fc5a316b472645
NCDK <mesterhakker@gmail.com>
2023-12-17 10:24:36 +0000

    ø

--- Githubtest.R
+++ Githubtest.R
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-# trying for the last time. 
 
+# trying for the last time. 
 # We are comitting this for GIT Gjsdfghj
-=======
-library(jsonlite)
-# Just a quick push to see if we can merge it like a G
-## huhuhuhuu
-# f
->>>>>>> e03f19579721bf5005e7e8afac200932de69d816
